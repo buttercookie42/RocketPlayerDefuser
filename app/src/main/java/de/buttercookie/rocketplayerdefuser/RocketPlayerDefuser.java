4d25b4f120d59f48b8b60a023550c0a94b731e70
Jan Henning <jh+vsc-hg@buttercookie.de>
2024-01-13 20:21:43 +0100

    Block 'Expired version' dialog

--- app/src/main/java/de/buttercookie/rocketplayerdefuser/RocketPlayerDefuser.java
+++ app/src/main/java/de/buttercookie/rocketplayerdefuser/RocketPlayerDefuser.java
@@ -4,12 +4,22 @@
 
 package de.buttercookie.rocketplayerdefuser;
 
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+import static de.robv.android.xposed.XposedHelpers.findClass;
+
+import android.os.Bundle;
+
 import androidx.annotation.Keep;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class RocketPlayerDefuser implements IXposedHookLoadPackage {
+
+    private static final String LOGTAG = "RocketPlayerDefuser";
+
+    private static final int EXPIRED_VERSION_DIALOG = 13;
 
     @Keep
     public RocketPlayerDefuser() {}
@@ -17,6 +27,25 @@ public class RocketPlayerDefuser implements IXposedHookLoadPackage {
     @Keep
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        if (!lpparam.packageName.equals("com.jrtstudio.AnotherMusicPlayer")) {
+            return;
+        }
 
+        final Class<?> targetClass =
+                findClass("com.jrtstudio.AnotherMusicPlayer.ActivityMusicBrowser", lpparam.classLoader);
+
+        findAndHookMethod("android.app.Activity", lpparam.classLoader,
+                "showDialog", int.class, Bundle.class, new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        if (!(param.thisObject.getClass() == targetClass)) {
+                            return;
+                        }
+
+                        if ((int) param.args[0] == EXPIRED_VERSION_DIALOG) {
+                            param.setResult(false);
+                        }
+                    }
+                });
     }
 }
